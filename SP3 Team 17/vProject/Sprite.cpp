1321bf3e9f7a348885c6d6d3ad64e3be6bc4d8fd
Eikatsu <hayashieikata@hotmail.co.jp>
2014-08-22 11:22:37 +0800

    Sprites
    Edited Sprites Class

--- SP3 Team 17/vProject/Sprite.cpp
+++ SP3 Team 17/vProject/Sprite.cpp
@@ -14,23 +14,47 @@ Sprite::~Sprite()
 {
 
 }
+
 bool Sprite::ImageInit(void)
 {
 	glEnable(GL_TEXTURE_2D);
-	//LoadTGA(&player[0], "images/");
-	//LoadTGA(&credit[0], "images/");
-	//LoadTGA(&particle[0], "images/");
-	//LoadTGA(&bag[0], "images/");
-	//LoadTGA(&note[0], "images/");
-	//LoadTGA(&mahjongfiend[0], "images/");
-	//LoadTGA(&pokerfiend[0], "images/");
-	//LoadTGA(&boss[0], "images/");
+
+	//Load Textures
+
+	Stop = true;
+	SubImage = 0;
+
+	glDisable(GL_TEXTURE_2D);
 	return true;
 }
 
 //softcode for sprites
 
-bool Sprite::LoadTGA(TextureImage *texture, char *filename)			// Loads A TGA File Into Memory
+void Sprite::render(int option)
+{
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBindTexture(GL_TEXTURE_2D, Images.texID);
+
+	glBegin(GL_QUADS);
+
+		//float ratio = 1/4;
+			glTexCoord2f(ratio * SubImage, 1); glVertex2f(0,0);
+			glTexCoord2f(ratio * SubImage, 0); glVertex2f(0,TILE_SIZE);
+			glTexCoord2f(ratio * (SubImage + 1), 0); glVertex2f(TILE_SIZE,TILE_SIZE);
+			glTexCoord2f(ratio * (SubImage + 1), 1); glVertex2f(TILE_SIZE,0);
+		
+	glEnd();
+
+	glDisable( GL_BLEND );
+	glDisable( GL_TEXTURE_2D );
+	glPopMatrix();
+}
+
+bool Sprite::LoadTGA(char *filename)			// Loads A TGA File Into Memory
 {    
 	GLubyte		TGAheader[12]={0,0,2,0,0,0,0,0,0,0,0,0};	// Uncompressed TGA Header
 	GLubyte		TGAcompare[12];								// Used To Compare TGA Header
@@ -56,28 +80,28 @@ bool Sprite::LoadTGA(TextureImage *texture, char *filename)			// Loads A TGA Fil
 		}
 	}
 
-	texture->width  = header[1] * 256 + header[0];			// Determine The TGA Width	(highbyte*256+lowbyte)
-	texture->height = header[3] * 256 + header[2];			// Determine The TGA Height	(highbyte*256+lowbyte)
+	Images.width  = header[1] * 256 + header[0];			// Determine The TGA Width	(highbyte*256+lowbyte)
+	Images.height = header[3] * 256 + header[2];			// Determine The TGA Height	(highbyte*256+lowbyte)
 
-	if(	texture->width	<=0	||								// Is The Width Less Than Or Equal To Zero
-		texture->height	<=0	||								// Is The Height Less Than Or Equal To Zero
+	if(	Images.width	<=0	||								// Is The Width Less Than Or Equal To Zero
+		Images.height	<=0	||								// Is The Height Less Than Or Equal To Zero
 		(header[4]!=24 && header[4]!=32))					// Is The TGA 24 or 32 Bit?
 	{
 		fclose(file);										// If Anything Failed, Close The File
 		return false;										// Return False
 	}
 
-	texture->bpp	= header[4];							// Grab The TGA's Bits Per Pixel (24 or 32)
-	bytesPerPixel	= texture->bpp/8;						// Divide By 8 To Get The Bytes Per Pixel
-	imageSize		= texture->width*texture->height*bytesPerPixel;	// Calculate The Memory Required For The TGA Data
+	Images.bpp	= header[4];							// Grab The TGA's Bits Per Pixel (24 or 32)
+	bytesPerPixel	= Images.bpp/8;						// Divide By 8 To Get The Bytes Per Pixel
+	imageSize		= Images.width*Images.height*bytesPerPixel;	// Calculate The Memory Required For The TGA Data
 
-	texture->imageData=(GLubyte *)malloc(imageSize);		// Reserve Memory To Hold The TGA Data
+	Images.imageData=(GLubyte *)malloc(imageSize);		// Reserve Memory To Hold The TGA Data
 
-	if(	texture->imageData==NULL ||							// Does The Storage Memory Exist?
-		fread(texture->imageData, 1, imageSize, file)!=imageSize)	// Does The Image Size Match The Memory Reserved?
+	if(	Images.imageData==NULL ||							// Does The Storage Memory Exist?
+		fread(Images.imageData, 1, imageSize, file)!=imageSize)	// Does The Image Size Match The Memory Reserved?
 	{
-		if(texture->imageData!=NULL)						// Was Image Data Loaded
-			free(texture->imageData);						// If So, Release The Image Data
+		if(Images.imageData!=NULL)						// Was Image Data Loaded
+			free(Images.imageData);						// If So, Release The Image Data
 
 		fclose(file);										// Close The File
 		return false;										// Return False
@@ -85,26 +109,26 @@ bool Sprite::LoadTGA(TextureImage *texture, char *filename)			// Loads A TGA Fil
 
 	for(GLuint i=0; i<int(imageSize); i+=bytesPerPixel)		// Loop Through The Image Data
 	{														// Swaps The 1st And 3rd Bytes ('R'ed and 'B'lue)
-		temp=texture->imageData[i];							// Temporarily Store The Value At Image Data 'i'
-		texture->imageData[i] = texture->imageData[i + 2];	// Set The 1st Byte To The Value Of The 3rd Byte
-		texture->imageData[i + 2] = temp;					// Set The 3rd Byte To The Value In 'temp' (1st Byte Value)
+		temp=Images.imageData[i];							// Temporarily Store The Value At Image Data 'i'
+		Images.imageData[i] = Images.imageData[i + 2];	// Set The 1st Byte To The Value Of The 3rd Byte
+		Images.imageData[i + 2] = temp;					// Set The 3rd Byte To The Value In 'temp' (1st Byte Value)
 	}
 
 	fclose (file);											// Close The File
 
 	// Build A Texture From The Data
-	glGenTextures(1, &texture[0].texID);					// Generate OpenGL texture IDs
+	glGenTextures(1, &Images.texID);					// Generate OpenGL texture IDs
 
-	glBindTexture(GL_TEXTURE_2D, texture[0].texID);			// Bind Our Texture
+	glBindTexture(GL_TEXTURE_2D, Images.texID);			// Bind Our Texture
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	// Linear Filtered
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	// Linear Filtered
 
-	if (texture[0].bpp==24)									// Was The TGA 24 Bits
+	if (Images.bpp==24)									// Was The TGA 24 Bits
 	{
 		type=GL_RGB;										// If So Set The 'type' To GL_RGB
 	}
 
-	glTexImage2D(GL_TEXTURE_2D, 0, type, texture[0].width, texture[0].height, 0, type, GL_UNSIGNED_BYTE, texture[0].imageData);
+	glTexImage2D(GL_TEXTURE_2D, 0, type, Images.width, Images.height, 0, type, GL_UNSIGNED_BYTE, Images.imageData);
 
 	return true;											// Texture Building Went Ok, Return True
 }
